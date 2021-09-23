0e21a562cde90b9b49ef963eb1cf6b103ca35bbe
Vika <queenviktoriyphp@gmail.com>
2021-09-23 15:28:29 +0300

    dropRight

---
+++ array/dropRight.js
@@ -0,0 +1,23 @@
+const dropRight = (array, n = 1) => {
+  let newArr = [];
+  let arrayReverded = array.reverse();
+  arrayReverded.forEach((e, index) => {
+    if (index >= n) {
+      newArr.unshift(e);
+    }
+  });
+
+  return newArr;
+};
+
+console.log(dropRight([1, 2, 3]));
+// => [1, 2]
+console.log(dropRight([1, 2, 3], 2));
+// => [1]
+console.log(dropRight([1, 2, 3], 5));
+// => []
+console.log(dropRight(["life", "is", "good", "fine"], 3));
+console.log(dropRight([1, 2, 3], 0));
+// => [1, 2, 3]
+
+module.exports = dropRight;
